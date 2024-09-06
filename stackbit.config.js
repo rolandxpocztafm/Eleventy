35e66b956bf4d0596345acaa279af578fe099887
rolandxpocztafm <rolandxpocztafm@gmail.com>
2024-09-06 22:20:17 +0200

    Update stackbit.config.js

--- stackbit.config.js
+++ stackbit.config.js
@@ -4,10 +4,16 @@ import { ContentfulContentSource } from "@stackbit/cms-contentful";
 export default {
   contentSources: [
     new ContentfulContentSource({
-      spaceId: process.env.CONTENTFUL_SPACE_ID,
-      environment: process.env.CONTENTFUL_ENVIRONMENT,
-      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
-      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN
+      spaceId: process.env.CONTENTFUL_SPACE_ID_01,
+      environment: process.env.CONTENTFUL_ENVIRONMENT_01,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN_01,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN_01
+    }),
+    new ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID_02,
+      environment: process.env.CONTENTFUL_ENVIRONMENT_02,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN_02,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN_02
     })
   ]
   // ...
